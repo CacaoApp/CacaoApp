e76f821813969445cfbb138f9576d660b71140e7
Jose Andrés Moncada <josemoncada87@hotmail.com>
2019-03-18 08:42:51 -0500

    delete router old

--- router.js
+++
@@ -1,7 +0,0 @@
-var url = new URL(window.location.href);
-var identificador = url.searchParams.get("cid");
-if (identificador === null) {
-  console.log("SIMPLE URL");
-} else {
-    location.href = "/site/jugador.html?cid=" + identificador;
-}
